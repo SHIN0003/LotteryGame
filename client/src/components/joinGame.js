9ab995014832f05ae1afd5e0d4a7927c63756026
Andrew Tai <andrewtai@umass.edu>
2024-02-17 16:06:02 -0500

    joinGame roomcode emit set up

--- client/src/components/joinGame.js
+++ client/src/components/joinGame.js
@@ -1,21 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 
-
-
-
+const socket = io('http://localhost:3000'); // Replace 'http://localhost:3000' with your server address
 
 const JoinGame = () => {
     const [roomCode, setRoomCode] = useState('');
-    io.socket.emit('join room', roomCode);
+
+    useEffect(() => {
+        if (roomCode.trim() !== '') {
+            socket.emit('join room', roomCode);
+        }
+    }, [roomCode]);
+
     return (
         <div>
-        <input type="text" value={roomCode} onChange={(e) => setRoomCode(e.target.value)} />
-            {/* <button onClick={}>Join Game</button> */}
+            <input
+                type="text"
+                value={roomCode}
+                onChange={(e) => setRoomCode(e.target.value)}
+            />
+            <button onClick={() => socket.emit('join room', roomCode)}>Join Game</button>
         </div>
     );
 };
-
-
 
 export default JoinGame;
--- server/index.js
+++ server/index.js
@@ -10,14 +10,29 @@ app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
+const roomCode = '3344'; // Function to generate a unique room code
+socket.join(roomCode);
+
+// Emit the room code to the big screen client
+io.to('bigScreenSocketId').emit('room code', roomCode);
+
+
 io.on('connection', (socket) => {
     console.log('A user connected with socket ID:', socket.id);
     //CHRIS IF U CAN SEE THIS THE LINE BELOW RECEIVES THE INPUT CAN YOU STORE IT TO A DATABASE
     // Listen for the 'user input' event from the client
+
+    socket.on('join room', (roomCode) => {
+      socket.join(roomCode);
+      // Notify the big screen and other players in the room...
+    });
+    
+
     socket.on('user input', (input) => {
       console.log('Received input from client with socket ID:', socket.id, 'Input:', input);
       
       const {socketId, dataArray} = input;
+
 
       //setSocketData(())
       
