ed94b6a9e85ff3b37866fd56edb009868544123b
Andrew Tai <andrewtai@umass.edu>
2024-02-17 16:33:45 -0500

    updated joingame.js to listen on game started

--- client/src/components/joinGame.js
+++ client/src/components/joinGame.js
@@ -1,16 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
-
+import PickNum from 'client/src/components/picknum.js';// Adjust the path as needed
 const socket = io('http://localhost:3001'); // Replace 'http://localhost:3000' with your server address
 
 const JoinGame = () => {
     const [roomCode, setRoomCode] = useState('');
+    const [gameStarted, setGameStarted] = useState(false);
 
     useEffect(() => {
-        if (roomCode.trim() !== '') {
-            socket.emit('join room', roomCode);
-        }
-    }, [roomCode]);
+        socket.on('game started', () => {
+          setGameStarted(true);
+        });
+      
+        // Clean up the listener when the component unmounts
+        return () => {
+          socket.off('game started');
+        };
+      }, []);
+
+      if (gameStarted) {
+        return <PickNum />;
+      }
 
     return (
         <div>
